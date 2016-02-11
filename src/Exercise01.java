88766d7b1d0b10acd4b93ea55e8158b26a0ab735
carolineevail <carolineevail@gmail.com>
2016-02-11 10:22:30 -0500

    Add Exercise 1

---
+++ src/Exercise01.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Caroline on 2/11/16.
+ */
+public class Exercise01 {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "David", "Edward", "Albert", "Beth"};
+        HashMap<Character, ArrayList<String>> nameMap = new HashMap<>();
+
+        //create empty arraylists
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            nameMap.put(firstLetter, new ArrayList<String>());
+
+        }
+
+        //add names; if you comment out the following for loop, it will pring blank letter arraylists.
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            nameMap.get(firstLetter).add(name);
+        }
+
+        System.out.println(nameMap);
+    }
+}
